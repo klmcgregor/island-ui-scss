7a15c561cb2c2d3706f668596cf02b98ce063a9d
Kyle L. McGregor <kyle@klmcgregor.com>
2021-04-23 12:45:06 -0400

    Add non-minified version CSS file

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const del = require('del');
 const shell = require('gulp-shell');
 const concat = require('gulp-concat');
 const cleanCss = require('gulp-clean-css');
+const rename = require('gulp-rename');
 
 const scssSrc = './src/styles/**/*.scss';
 const cssDest = './lib';
@@ -18,8 +19,10 @@ gulp.task('styles', () => {
   return gulp.src('./src/styles/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(concat('island-ui.css'))
+    .pipe(gulp.dest(cssDest)) // This will output the non-minified version
+    .pipe(rename({ extname: '.min.css' }))
     .pipe(cleanCss())
-    .pipe(gulp.dest(cssDest));
+    .pipe(gulp.dest(cssDest)); // This will output the minified version
 });
 
 gulp.task('cleanSCSS', () => {
